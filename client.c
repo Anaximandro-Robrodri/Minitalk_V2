9545b7d37846ebcffdd6287824d8cdf18c4856d9
user42 <karmmero@gmail.com>
2021-08-04 00:23:01 +0200

    It works

--- client.c
+++ client.c
@@ -1,6 +1,6 @@
 #include "minitalk.h"
 
-int	ft_int_len(long n)
+/*static int	ft_int_len(long n)
 {
 	int	i;
 
@@ -32,8 +32,16 @@ static char		*ft_binary(char ptr)
 		holder += div % 2;
 		div /= 2;
 	}
+	if (ft_int_len(holder) == 7)
+	{
+		ret = ft_reverse_nbr(holder);
+		ft_putstr(ret);
+		return (ret);
+	}
 	while (ft_int_len(holder) < 7)
 		holder *= 10;
+	ft_putnbr(holder);
+	write(1, "\n", 1);
 	ret = ft_itoa(holder);
 	return (ret);
 }
@@ -53,11 +61,9 @@ static void	ft_send_signal(char **argv, int argc)
 		{
 			binary = ft_binary(*str);
 			i = 0;
-			ft_putstr(binary);
-			write (1, "\n", 1);
 			while (binary[i])
 			{
-				sleep(1);
+//				sleep(1);
 				if (binary[i++] == 48)
 					kill(pid, SIGUSR1);
 				else
@@ -68,12 +74,36 @@ static void	ft_send_signal(char **argv, int argc)
 		}
 	}
 }
+*/
+void	ft_send_signal(char **argv)
+{
+	int	pid;
+	char	*str;
+	int	shift;
+
+	pid = ft_atoi(argv[1]);
+	str = argv[2];
+	while (*str)
+	{
+		shift = 0;
+		while (shift < 7)
+		{
+			if (((*str >> shift) & 1))
+				kill(pid, SIGUSR2);
+			else
+				kill(pid, SIGUSR1);
+		 	shift++;	 
+			usleep(50);
+		}
+		str++;
+	}
+}
 
 int main (int argc, char **argv)
 {
 	if (argc == 3)
 	{
-		ft_send_signal(argv, argc);
+		ft_send_signal(argv);
 	}
 	else
 		ft_putstr("You must include server PID and a message\n");
--- server.c
+++ server.c
@@ -9,13 +9,15 @@ void	ft_init_struct()
 	holder.size = 0;
 }
 
-static	char	ft_decimal(int	nbr)
+static	char	ft_decimal(char	*msg)
 {
+	int	nbr;
 	int	holder;
 	int	dec;
 	char	ret;
 	int	base;
 
+	nbr = ft_atoi(msg);
 	holder = 0;
 	dec = 0;
 	base = 1;
@@ -30,46 +32,54 @@ static	char	ft_decimal(int	nbr)
 	return (ret);
 }
 
-/*void	ft_realloc()
+static void	ft_invert_nbr()
 {
-	holder.msg = malloc(holder.pos * sizeof(char));
-	if (holder.msg == NULL)
+	char	tmp;
+	int	i;
+	int	len;
+	int	div;
+
+	i = 0;
+	len = ft_strlen(holder.msg);
+	div = len / 2;
+	while (i < div)
 	{
-		ft_putstr("Error\n");
-		exit(-1);
+		tmp = holder.msg[i];
+		holder.msg[i] = holder.msg[len - i - 1];
+		holder.msg[len - i - 1] = tmp;
+		i++;
 	}
-	holder.msg[holder.pos + 1] = '\0';
-}*/
+}
 
 void	sig_handler(int signal)
 {
-	if (signal == SIGUSR1)
+/*	if (signal == SIGUSR1)
 	{
 		holder.pos *= 10;
-		holder.pos += 0;
 		holder.size++;
 	}
-	else if (signal == SIGUSR2)
+	else if (signal == SIGUSR2)			0 * 0 es 0, normal que no funcione no?
 	{
 		holder.pos *= 10;
 		holder.pos += 1;
 		holder.size++;
-	}
-/*	ft_realloc();
+	}*/
+	if (holder.msg == NULL)
+		holder.msg = ft_calloc(7, sizeof(char));
 	if (signal == SIGUSR1)
 	{
-		holder.msg[holder.pos] = 48;		LA IDEA ERA USAR UNA CADENA. PUEDE QUE LA RETOME
+		holder.msg[holder.pos] = 48;
 		holder.pos++;
 	}
 	else if (signal == SIGUSR2)
 	{
 		holder.msg[holder.pos] = 49;
 		holder.pos++;
-	}*/
-	if (holder.size == 7)
+	}
+	if (holder.pos == 7)
 	{
-		ft_putchar(ft_decimal(holder.pos));
-		holder.size = 0;
+		ft_invert_nbr();
+		ft_putchar(ft_decimal(holder.msg));
 		holder.pos = 0;
 	}
 }
