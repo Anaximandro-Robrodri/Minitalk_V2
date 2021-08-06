f5b0fc74174f75144c68da1099d58b89a2114c8f
Roberto Rodriguez garcia <robrodri@c1r12s1.42madrid.com>
2021-08-06 11:22:39 +0200

    works on mac

--- client.c
+++ client.c
@@ -1,81 +1,5 @@
 #include "minitalk.h"
 
-/*static int	ft_int_len(long n)
-{
-	int	i;
-
-	i = 1;
-	if (n < 0)
-	{
-		n *= -1;
-		i++;
-	}
-	while (n > 9)
-	{
-		n = n / 10;
-		i++;
-	}
-	return (i);
-}
-
-static char		*ft_binary(char ptr)
-{
-	int		div;
-	int		holder;
-	char	*ret;
-
-	div = (int)ptr;
-	holder = 0;
-	while (div > 0)
-	{
-		holder *= 10;
-		holder += div % 2;
-		div /= 2;
-	}
-	if (ft_int_len(holder) == 7)
-	{
-		ret = ft_reverse_nbr(holder);
-		ft_putstr(ret);
-		return (ret);
-	}
-	while (ft_int_len(holder) < 7)
-		holder *= 10;
-	ft_putnbr(holder);
-	write(1, "\n", 1);
-	ret = ft_itoa(holder);
-	return (ret);
-}
-
-static void	ft_send_signal(char **argv, int argc)
-{
-	int			pid;
-	int			i;
-	char		*str;
-	char		*binary;
-
-	pid = ft_atoi(argv[1]);
-	str = argv[2];
-	while (--argc)
-	{
-		while(*str)
-		{
-			binary = ft_binary(*str);
-			i = 0;
-			while (binary[i])
-			{
-//				sleep(1);
-				if (binary[i++] == 48)
-					kill(pid, SIGUSR1);
-				else
-					kill(pid, SIGUSR2);
-			}
-			str++;
-			free(binary);
-		}
-	}
-}
-*/
-
 static void	ft_send_new_line(int pid)
 {
 	char	*binary;
@@ -88,7 +12,7 @@ static void	ft_send_new_line(int pid)
 		else
 			kill (pid, SIGUSR2);
 		binary++;
-//		usleep(100);
+		usleep(50);
 	}
 }
 
@@ -107,17 +31,11 @@ static void	ft_send_signal(int pid, char *str)
 		while (shift >= 0)
 		{
 			if (((*str >> shift) & 1))
-			{
-				ft_putnbr(1);
 				kill(pid, SIGUSR2);
-			{
 			else
-			{
-				ft_putnbr(0);
 				kill(pid, SIGUSR1);
-			}
 		 	shift--;	 
-//			usleep(100);
+			usleep(50);
 		}
 		str++;
 	}
